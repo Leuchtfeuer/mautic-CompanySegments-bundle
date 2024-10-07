7d97aa99b7b674947436f9d71e668d29525f2fc6
lenonleite <lenonleite@gmail.com>
2024-10-07 13:01:44 +0100

    - add API - add Tests - fix database entity name - add permissions

--- Validator/Constraints/CircularDependencyValidator.php
+++ Validator/Constraints/CircularDependencyValidator.php
@@ -34,7 +34,6 @@ class CircularDependencyValidator extends ConstraintValidator
         }
 
         $dependentSegmentIds = $this->flatten(array_map(function ($id): array {
-            $id     = (int) $id;
             $entity = $this->model->getEntity($id);
             assert($entity instanceof CompanySegment);
 
