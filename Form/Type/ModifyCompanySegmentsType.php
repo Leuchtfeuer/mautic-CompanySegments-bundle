8c9169aec173e3ddff66337bd3998b456fa8d420
Jonas <j.ludwig@leuchtfeuer.com>
2026-02-03 14:57:41 +0100

    MTC-9771 CS Fixer

--- Form/Type/ModifyCompanySegmentsType.php
+++ Form/Type/ModifyCompanySegmentsType.php
@@ -12,6 +12,7 @@ class ModifyCompanySegmentsType extends AbstractType
         private TranslatorInterface $translator
     ) {
     }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add(
